bf81b8810b1c508c8ceefa682a533180df219a34
Иванов Евгений <ivanov.eugenea@yandex.ru>
2020-03-29 15:58:22 +0400

    complete Hello Triangle Lesson

--- habrTutor/TriangleCreation/main.cpp
+++ habrTutor/TriangleCreation/main.cpp
@@ -34,6 +34,7 @@ void checkProgramLinking(GLuint& program);
 
 int main()
 {
+	//------------------------------------------------------------------------------
 	//Инициализация GLFW
 	glfwInit();
 	//Настройка GLFW
@@ -73,31 +74,7 @@ int main()
 	glViewport(0, 0, width, height);
 
 	glfwSetKeyCallback(window, key_callback);
-
-	float vertices[] = {
-	-1.0f, -0.5f, 0.0f,
-	 0.5f, -0.5f, 0.0f,
-	 1.0f,  0.5f, 0.0f
-	};
-	
-	GLuint VAO;
-	glGenVertexArrays(1, &VAO);
-	glBindVertexArray(VAO);
-
-	//send to GPU input vertex data
-	// pointer to our data in GPU's memory also it can interpret as unic ID of OpenGL's object
-	GLuint VBO; 
-	//parameters (1,&VBO) : "1" is number of requered object(buffer) , &VBO has pointer to new allocated data for new object(buffer)
-	//memory allocation under vertices data
-	glGenBuffers(1, &VBO); 
-	// GL_ARRAY_BUFFER is type of vertex data buffer, 
-	// it bind specified buffer type to newly created buffer
-	// GL_ARRAY_BUFFER associated with VBO. All manipulating with GL_ARRAY_BUFFER will be ref to VBO
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	// here we specify type of targeted bufer, then it's size, next data, at the end mode
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	//instructing of GPU how it should process it
+	//------------------------------------------------------------------------------
 	GLuint vertexShader;
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
@@ -120,17 +97,42 @@ int main()
 
 	checkProgramLinking(shaderProgram);
 
-	
-
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
+	//------------------------------------------------------------------------------
+	GLfloat vertices[] = {
+	 0.5f,  0.5f, 0.0f,  // Верхний правый угол
+	 0.5f, -0.5f, 0.0f,  // Нижний правый угол
+	-0.5f, -0.5f, 0.0f,  // Нижний левый угол
+	-0.5f,  0.5f, 0.0f   // Верхний левый угол
+	};
+	GLuint indices[] = {  // Помните, что мы начинаем с 0!
+		0, 1, 3,   // Первый треугольник
+		1, 2, 3    // Второй треугольник
+	};
+	//------------------------------------------------------------------------------
+	//create indetificators for objects
+	GLuint VAO , VBO, EBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+	//------------------------------------------------------------------------------
+	glBindVertexArray(VAO);//start working with VAO
 
-	//say to OpenGL how it should iterprete vertexes data
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), (GLvoid*)0);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
 
-	glBindVertexArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
+	glBindVertexArray(0);//end working with VAO
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	//------------------------------------------------------------------------------
 	while (!glfwWindowShouldClose(window))
 	{
 		//check callback function
@@ -142,13 +144,16 @@ int main()
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glDrawArrays(GL_TRIANGLES, 0, 6);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
 		//swap
 		glfwSwapBuffers(window);
 	}
 
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 
 	glfwTerminate();
 	
